0c3d38266ea43bff1f06332fd079e2d87897516f
Guilherme Fernandes <143087629+DrizinCoder@users.noreply.github.com>
2025-05-07 09:44:14 -0300

    ⚙️ refactor: Atualiza header

--- LibCodes/matrixASM.h
+++ LibCodes/matrixASM.h
@@ -1,28 +1,28 @@
 #ifndef MATRIXASM_H
 #define MATRIXASM_H
 
-void writeMatrix(char *m_pointer_a, char *m_pointer_b, int size, int *address);
-
+// Operações básicas
+void writeMatrixA(char *m_pointer_a, int size, int *address);
+void writeMatrixB(char *m_pointer_b, int size, int *address);
 void readMatrix(int size, int *address);
 
-void sum();
+// Operações matriciais
+void sum(int *address);
+void sub(int *address);
+void mul(int *address);
+void scalar_mul(int *address);
+void oposite(int *address);
+void transpose(int *address);
 
-void sub();
+// Cálculo de determinantes
+void det2(int *address);
+void det3(int *address);
+void det4(int *address);
+void det5(int *address);
 
-void mul();
-
-void scalar_mul();
-
-void oposite();
-
-void transpose();
-
-void det2();
-
-void det3();
-
-void det4();
-
-void det5();
+// Auxiliares
+int open_memory();
+int* map_memory(unsigned int address, unsigned int size);
+void unmap_memory(int *mapped_address, unsigned int size);
 
 #endif
