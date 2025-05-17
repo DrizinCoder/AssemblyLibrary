d2186ce3a721ee4202633d6121624287ff2b73b8
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-05-17 09:16:44 -0300

    refactor: remove pastas que não fazem parte do projeto

--- code/examples/sum/sum.s
+++
@@ -1,9 +0,0 @@
-    .section .text
-    .global sum         # Torna a função 'sum' visível para o código C
-
-sum:
-    # Entrada: %edi = primeiro número, %esi = segundo número
-    # Saída: %eax = soma
-    movl %edi, %eax     # Copia primeiro número (%edi) para %eax
-    addl %esi, %eax     # Soma segundo número (%esi) a %eax
-    ret                 # Retorna (resultado em %eax)
