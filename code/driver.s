a141046bd9d3f8830df8f40d5fe2338ce15b373a
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-05-18 11:55:27 -0300

    feat: melhora estrutura e organiza carregamentos

--- code/driver.s
+++ code/driver.s
@@ -27,43 +27,97 @@ driver:
     str r2, [r4]      @ matrixR pointer
     ldr r4, =matrix_size
     str r3, [r4]
-    ldr r4, [sp, #16]
+    ldr r4, [sp, #24]
     ldr r5, =opcode
+    str r4, [r5]
 
     bl mmap_setup
     bl welcome
-
     bl load
     bl operation
     bl store
     
-    pop {r4-r8, pc} 
+    pop {r4-r8, lr} 
+    bx lr
 
 load:
     push {lr}
 
-    pop {br}
+    ldr r0, =matrix_size
+    ldr r0, [r0]
+
+    cmp r0, #0
+    beq load2x2
+
+    cmp r0, #1
+    beq load3x3
+
+    cmp r0, #2
+    beq load4x4
+
+    cmp r0, #3
+    beq load5x5
+
+    pop {lr}
+    bx lr
+
+load2x2:
+    push {lr}
+
+
+    @ code [pega os 4 valores de 2 em 2 e fazer LOAD] ...
+    
+    pop {lr}
+    bx lr
+
+load3x3:
+    push {lr}
+
+    @ code ...
+
+    pop {lr}
+    bx lr
+
+load4x4:
+    push {lr}
+
+    @ code ...
+
+    pop {lr}
+    bx lr
+
+load5x5:
+    push {lr}
+
+    @ code ...
+
+    pop {lr}
+    bx lr
 
 operation:
     push {lr}
 
-    pop {br}
+    @ code ...
+
+    pop {lr}
+    bx lr
 
 store:
     push {lr}
 
-    pop {br}
+    @ code ...
+
+    pop {lr}
+    bx lr
 
 welcome:
-    push {lr}
-
     mov r7, #4        @ syscall write
     mov r0, #1        @ stdout
     ldr r1, =welcome
     mov r2, #welcome_len
     svc #0
-     
-    pop{pc}
+
+    bx lr
 
 mmap_setup:
     push {lr}
@@ -97,11 +151,13 @@ mmap_setup:
     ldr r1, =mapped_addr
     str r0, [r1]
     
-    pop {pc}
+    pop {lr}
+    bx lr
 
 fail_open:
     mov r0, #-1
-    pop {pc}
+
+    bx lr
 
 fail_mmap:
     @ Close file if mmap failed
@@ -111,5 +167,5 @@ fail_mmap:
     svc #0
     
     mov r0, #-1
-    pop {pc}
-    
+
+    bx lr
